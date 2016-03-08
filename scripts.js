bcc5b11efffb35f411d5ace01f80c4aa6b29f333
5adiyah <sadiali@pdx.edu>
2016-03-07 18:50:06 -0800

    Take string, turn to int, convert binary to decimal

--- scripts.js
+++ scripts.js
@@ -6,6 +6,20 @@ var converter = function(number) {
     output.push(+sNumber.charAt(i));
     console.log(output);
   }
-  
+  debugger
+  for(var j = 0; j < output.length; j++){
+    iNumber = parseInt(output[j]);
+    if(output[j] === 0){
+      convertednum = (convertednum  * 2);
+    } else if (output[j]=== 1){
+      convertednum = ((convertednum * 2) + 1);
+    }
+  }
+  return convertednum;
+};
+
+$(document).ready(function() {
+  var number=["1111"];
+  converter(number);
 
 });
