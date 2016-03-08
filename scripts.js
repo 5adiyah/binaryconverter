90ca487318f05ee75426ff7b22e890866685c7a6
5adiyah <sadiali@pdx.edu>
2016-03-07 18:49:32 -0800

    Take number, convert to string

--- scripts.js
+++ scripts.js
@@ -1,4 +1,11 @@
 var converter = function(number) {
-
+  output = [];
+  var convertednum = 0;
+  sNumber = number.toString();
+  for (var i = 0; i < sNumber.length; i++){
+    output.push(+sNumber.charAt(i));
+    console.log(output);
+  }
+  
 
 });
