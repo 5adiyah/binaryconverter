30eb188a2b00419184ccf0b789fa2519f474f281
5adiyah <sadiali@pdx.edu>
2016-03-07 19:33:22 -0800

    Add user part

--- scripts.js
+++ scripts.js
@@ -2,11 +2,12 @@ var converter = function(number) {
   output = [];
   var convertednum = 0;
   sNumber = number.toString();
+
   for (var i = 0; i < sNumber.length; i++){
     output.push(+sNumber.charAt(i));
     console.log(output);
   }
-  debugger
+
   for(var j = 0; j < output.length; j++){
     iNumber = parseInt(output[j]);
     if(output[j] === 0){
@@ -18,8 +19,30 @@ var converter = function(number) {
   return convertednum;
 };
 
-$(document).ready(function() {
-  var number=["1111"];
-  converter(number);
+$(document).ready(function(){
+  $("button#convert").click(function(){
+    $("#binaryconverter").hide();
+    $("#clear").show();
+    $("#result").show();
+    var number = parseInt($("input#number").val());
+    var finalnumber = converter(number);
+    $(".sentence").text(finalnumber);
+    event.preventDefault();
+  });
+
+  $("button#clear").click(function(){
+    $("#binaryconverter").show();
+    $("#clear").hide();
+    $("#result").hide();
+    event.preventDefault();
+  });
+
+  $("#binaryconverter").submit(function(){
+    var number = parseInt($("input#number").val());
+    var finalnumber = converter(number);
+    // $(".sentence").text("hello");
+  });
+
+  event.preventDefault();
 
 });
